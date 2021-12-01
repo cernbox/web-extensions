215ed028c8059bb648f2f07e566e00d8c0896f08
Diogo Castro <diogo.castro@cern.ch>
2021-12-01 13:38:57 +0100

    clean when building all

--- buildall.sh
+++ buildall.sh
@@ -1,1 +1,1 @@
-ls -d ./*/ | xargs -n1 sh -c 'if [ -e $0Makefile ]; then echo "Building $0" ; cd $0 ; make release; fi'
+ls -d ./*/ | xargs -n1 sh -c 'if [ -e $0Makefile ]; then echo "Building $0" ; cd $0 ; make clean ; make release; fi'
