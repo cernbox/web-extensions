3ffefa9115227cced6cd4c58059f2dc231b33e27
Diogo Castro <diogo.castro@cern.ch>
2022-06-16 19:28:46 +0200

    old-web-redirector: rewrite urls to ocis

---
+++ old-web-redirector/src/Error.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="oc-position-center">
+      <div class="oc-text-center">
+        <oc-icon size="xxlarge" name="error-warning" fill-type="line" />
+        <p class="oc-text-lead">This URL is no longer valid</p>
+        <p>Please contact the support if you think this is an error</p>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+class UrlError extends Error {
+  constructor(url) {
+    super(`The url '${url}' is not registered`)
+    this.name = 'UrlError'
+  }
+}
+
+export default {
+  name: 'Redirector',
+
+  mounted() {
+    throw new UrlError(this.$route.fullPath)
+  }
+}
+</script>
