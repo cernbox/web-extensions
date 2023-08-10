7d8ad7207d4a84f734a38b055048eac88db87e0e
Elizaveta Ragozina <elizaveta.ragozina@cern.ch>
2023-08-10 09:36:24 +0200

    survey: extension for linking a usability survey

---
+++ survey/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <oc-button id="survey" appearance="raw" variation="inverse" href="https://indico.cern.ch/event/1315215/" target="_blank" type="a" v-oc-tooltip="'Usability survey for CERNBox Web'">
+      <oc-icon name="survey" />
+      <span>Survey</span>
+    </oc-button>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
