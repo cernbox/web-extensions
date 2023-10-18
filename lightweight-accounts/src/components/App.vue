db525beae12ea103bb429610b61c7654678ef3ca
Jannik Stehle <jannik.stehle@gmail.com>
2023-10-18 10:54:16 +0200

    feat: lightweight account home page

---
+++ lightweight-accounts/src/components/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="oc-text-center oc-flex oc-flex-middle oc-p-m">
+    <div class="oc-width-1-1">
+      <h1 v-translate class="oc-text-lead">Welcome to CERNBox</h1>
+      <p v-translate>With this account you can access shared content and collaborate on projects</p>
+    </div>
+  </div>
+</template>
