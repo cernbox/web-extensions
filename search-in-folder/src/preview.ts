b3631cb3a0c38d9804a387e4d00f95c20558b7be
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-02-17 11:21:38 +0100

    search-in-folder: update extension

--- search-in-folder/src/preview.ts
+++ search-in-folder/src/preview.ts
@@ -1,15 +1,14 @@
-import { filterResources, ResourcePreview, SearchPreview, SearchResult } from '@ownclouders/web-pkg'
+import { filterResources, ResourcePreview, ResourcesStore, SearchPreview, SearchResult } from '@ownclouders/web-pkg'
 import { Resource } from '@ownclouders/web-client'
-import { Store } from 'vuex'
 import { Component, unref } from 'vue'
 import { Router } from 'vue-router'
 
 export default class Preview implements SearchPreview {
   public readonly component: Component
   private readonly router: Router
-  private readonly store: Store<any>
+  private readonly store: ResourcesStore
 
-  constructor(store: Store<any>, router: Router) {
+  constructor(store: ResourcesStore, router: Router) {
     this.component = ResourcePreview
     this.router = router
     this.store = store
@@ -28,8 +27,8 @@ export default class Preview implements SearchPreview {
 
     // no cache required, the filtering is client only and fast enough to recalculate the set
     // of results every time on the fly
-    const resources = filterResources(this.store.getters['Files/files'] as Resource[], term, 5)
-    const areHiddenFilesShown: boolean = this.store.state.Files?.areHiddenFilesShown
+    const resources = filterResources(this.store.resources as Resource[], term, 5)
+    const areHiddenFilesShown: boolean = this.store.areHiddenFilesShown
 
     const searchResult = resources.reduce((acc, resource) => {
       // filter results if hidden files shouldn't be shown due to settings
--- search-in-folder/src/provider.ts
+++ search-in-folder/src/provider.ts
@@ -1,9 +1,8 @@
-import { SearchList, SearchPreview, SearchProvider } from '@ownclouders/web-pkg'
+import { ResourcesStore, SearchList, SearchPreview, SearchProvider } from '@ownclouders/web-pkg'
 import Preview from './preview'
-import { Store } from 'vuex'
 import { Router } from 'vue-router'
 
-function $gettext(msg) {
+function $gettext(msg: string) {
   return msg
 }
 
@@ -12,9 +11,9 @@ export class Provider implements SearchProvider {
   public readonly displayName: string
   public readonly previewSearch: SearchPreview
   public readonly listSearch: SearchList
-  private readonly store: Store<any>
+  private readonly store: ResourcesStore
 
-  constructor(store: Store<any>, router: Router) {
+  constructor(store: ResourcesStore, router: Router) {
     this.id = 'files.filter'
     this.displayName = $gettext('In this folder')
     this.store = store
@@ -22,6 +21,6 @@ export class Provider implements SearchProvider {
   }
 
   public get available() {
-    return !!this.store.getters['Files/currentFolder']
+    return !!this.store.currentFolder
   }
 }
