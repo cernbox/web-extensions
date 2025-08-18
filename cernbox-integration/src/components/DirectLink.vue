f565a1a6e916e85ce652ef9cca1b1aef06c01ccc
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-08-18 10:55:24 +0200

    cernbox-integration: fix direct link for shares

--- cernbox-integration/src/components/DirectLink.vue
+++ cernbox-integration/src/components/DirectLink.vue
@@ -1,23 +1,36 @@
 <template>
-  <div class="oc-flex oc-flex-middle oc-flex-between oc-width-1-1 oc-p-xs oc-mb-m link-name-container">
+  <div
+    class="oc-flex oc-flex-middle oc-flex-between oc-width-1-1 oc-p-xs oc-mb-m link-name-container"
+  >
     <div class="oc-flex oc-flex-middle oc-text-truncate">
       <oc-icon name="link" fill-type="line" />
       <p class="oc-files-file-link-url oc-ml-s oc-text-truncate oc-my-rm" v-text="directLink" />
     </div>
-    <oc-button v-if="isClipboardCopySupported" v-oc-tooltip="$gettext('Copy')" class="oc-ml-s" size="small"
-      :aria-label="$gettext('Copy')" @click="copyLinkToClipboard">
+    <oc-button
+      v-if="isClipboardCopySupported"
+      v-oc-tooltip="$gettext('Copy')"
+      class="oc-ml-s"
+      size="small"
+      :aria-label="$gettext('Copy')"
+      @click="copyLinkToClipboard"
+    >
       <span v-text="$gettext('Copy')" />
     </oc-button>
   </div>
 </template>
 <script lang="ts">
 import { computed, defineComponent, unref } from 'vue'
-import { Resource, SpaceResource } from '@ownclouders/web-client'
+import { Resource, SpaceResource, urlJoin } from '@ownclouders/web-client'
 import { PropType } from 'vue'
 import { useClipboard } from '@vueuse/core'
-import { createFileRouteOptions, useConfigStore, useMessages, useRouter } from '@ownclouders/web-pkg'
+import {
+  createFileRouteOptions,
+  useAuthStore,
+  useConfigStore,
+  useMessages,
+  useRouter
+} from '@ownclouders/web-pkg'
 import { useGettext } from 'vue3-gettext'
-import { urlJoin } from '@ownclouders/web-client'
 
 export default defineComponent({
   props: {
@@ -25,18 +38,20 @@ export default defineComponent({
     resource: { type: Object as PropType<Resource>, required: true }
   },
   setup(props) {
+    const authStore = useAuthStore()
     const configStore = useConfigStore()
     const messageStore = useMessages()
+
+    const isPublicLinkContext = authStore.publicLinkContextReady
 
     const router = useRouter()
     const { $gettext } = useGettext()
 
     const serverUrl = computed(() => configStore.serverUrl)
 
-    const directLink = computed(() => {
-      const routeOpts = createFileRouteOptions(props.space, props.resource)
-      return urlJoin(unref(serverUrl), router.resolve(routeOpts).fullPath)
-    })
+    const directLink = !unref(isPublicLinkContext)
+      ? props.resource.privateLink
+      : urlJoin(unref(serverUrl), props.resource.downloadURL)
 
     const { copy, isSupported: isClipboardCopySupported } = useClipboard({
       legacy: true,
