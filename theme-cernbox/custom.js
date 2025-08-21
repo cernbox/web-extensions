ee0769ef15269f773cd48fdfb69e3ee789c960aa
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-08-21 09:12:10 +0200

    theme-cernbox: remove label
    the label is not properly placed after the style is loaded.
    being just a redundancy, is shouldn't be a problem removing it.

--- theme-cernbox/custom.js
+++ theme-cernbox/custom.js
@@ -7,38 +7,18 @@ if (appsRegex.test(window.location.pathname)) {
 }
 
 const root = document.querySelector(":root");
-const flavorLabel = document.createElement("div");
-flavorLabel.classList.add("oc-font-bold");
-flavorLabel.style.fontSize = "var(--oc-font-size-xlarge)";
-flavorLabel.style.color = "var(--oc-color-swatch-brand-contrast)";
 
 const qaBoxRegex = /^qa\./i;
-const qaEnv = qaBoxRegex.test(window.location.hostname);
-if (qaEnv) {
-  flavorLabel.innerText = "QA";
+const testBoxRegex = /^test\./i;
+const devBoxRegex = /^cbox-ocisdev-([A-z]*)\./i;
+
+if (qaBoxRegex.test(window.location.hostname)) {
   root.style.setProperty("--oc-color-swatch-brand-default", "#7F1734");
   root.style.setProperty("--oc-color-swatch-brand-hover", "#A01C41");
-}
-
-const testBoxRegex = /^test\./i;
-const testEnv = testBoxRegex.test(window.location.hostname);
-if (testEnv) {
-  flavorLabel.innerText = "TEST";
+} else if (testBoxRegex.test(window.location.hostname)) {
   root.style.setProperty("--oc-color-swatch-brand-default", "#004225");
   root.style.setProperty("--oc-color-swatch-brand-hover", "#1E4D2B");
-}
-
-const devBoxRegex = /^cbox-ocisdev-([A-z]*)\./i;
-const devEnv = devBoxRegex.test(window.location.hostname);
-if (devEnv) {
-  flavorLabel.innerText = "DEV";
+} else if (devBoxRegex.test(window.location.hostname)) {
   root.style.setProperty("--oc-color-swatch-brand-default", "#008B8B");
   root.style.setProperty("--oc-color-swatch-brand-hover", "#00AAAA");
 }
-
-setTimeout(() => {
-  const headerLabels = document.querySelector(
-    ".oc-topbar-left.oc-flex.oc-flex-middle.oc-flex-start",
-  );
-  headerLabels.appendChild(flavorLabel);
-}, 100);
