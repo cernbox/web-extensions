e5123934a45e77bea41ad5377e8ac3f5fc7a4160
Diogo Castro <diogo.castro@cern.ch>
2025-11-12 19:09:21 +0100

    rootjs: improvements and cleanups
    Adds possibility to configure the iframe url.
    Personalises the ROOT UI for a more embedded mode.

--- rootjs/src/App.vue
+++ rootjs/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <iframe id="rootjs-viewer" ref="rootJsViewer" :src="iframeSource" title="ROOTJS Viewer" />
 </template>
-<script type="module"></script>
 <script lang="ts">
-import { defineComponent, unref, ref, onMounted } from 'vue'
+import qs from 'qs'
+import { defineComponent, unref, ref, onMounted, PropType } from 'vue'
 import { Resource, SpaceResource } from '@ownclouders/web-client'
-import { useClientService, useAuthStore, useUserStore } from '@ownclouders/web-pkg'
+import { useClientService, useAuthStore, useUserStore, AppConfigObject } from '@ownclouders/web-pkg'
 
 export default defineComponent({
   name: 'ROOTJSViewer',
@@ -17,14 +17,21 @@ export default defineComponent({
     resource: {
       type: Resource,
       required: true
-    }
+    },
+    applicationConfig: {
+      type: Object as PropType<AppConfigObject>,
+      required: true,
+      // hack so the correct type is being used for the app config
+      default: (): AppConfigObject => undefined
+    },
   },
   setup(props) {
     const clientService = useClientService()
     const { user } = useUserStore()
     const authStore = useAuthStore()
 
-    const rootUrl = 'https://root.cern/js/latest'
+    const { url = 'https://root.cern/js/latest/' } = props.applicationConfig
+
     const iframeSource = ref('')
 
     onMounted(async () => {
@@ -34,20 +41,21 @@ export default defineComponent({
         username: user.id
       })
       const fileUrl = new URL(signedUrl)
-      fileUrl.search = ''
-      iframeSource.value = `${rootUrl}?file=${fileUrl.href}?access_token=${authStore.accessToken}`
+      fileUrl.search = '' // remmove signature...
+
+      const query = qs.stringify({
+        // file: `${fileUrl.href}?access_token=${authStore.accessToken}`,
+        noselect: 'file',
+        topname: props.resource.name,
+        info: 'ROOT viewer',
+      })
+      iframeSource.value = `${url}?${query}&file=${fileUrl.href}?access_token=${authStore.accessToken}`
+      // iframeSource.value = `${url}?${query}`
     })
 
     return {
       iframeSource
     }
-  },
-  methods: {
-    showError: function () {
-      console.error('showError')
-    }
   }
 })
 </script>
-
-<style></style>
