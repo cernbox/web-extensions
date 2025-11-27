8e7eb291236de9cfa554d69793baefc0ae7b23bc
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-11-27 11:06:56 +0100

    draw-io: duplicate openEmptyEditor function
    to be reverted once 'forceSameTab' param is pushed upstream

--- draw-io/src/App.vue
+++ draw-io/src/App.vue
@@ -8,20 +8,36 @@
 </template>
 
 <script lang="ts">
+import { isShareSpaceResource, Resource, SpaceResource, urlJoin } from '@ownclouders/web-client'
+import {
+  AppConfigObject,
+  ApplicationFileExtension,
+  EDITOR_MODE_EDIT,
+  resolveFileNameDuplicate,
+  useAppsStore,
+  useClientService,
+  useConfigStore,
+  useFileActions,
+  useGetMatchingSpace,
+  useResourcesStore,
+  useRoute,
+  useRouter,
+  useSpacesStore,
+  useThemeStore,
+  useWindowOpen
+} from '@ownclouders/web-pkg'
 import qs from 'qs'
 import {
-  defineComponent,
-  unref,
-  PropType,
   computed,
-  onMounted,
+  defineComponent,
+  nextTick,
   onBeforeUnmount,
+  onMounted,
+  PropType,
   ref,
-  watch,
-  nextTick
+  unref,
+  watch
 } from 'vue'
-import { Resource } from '@ownclouders/web-client'
-import { AppConfigObject, useThemeStore } from '@ownclouders/web-pkg'
 import { useGettext } from 'vue3-gettext'
 
 export default defineComponent({
@@ -55,6 +71,20 @@ export default defineComponent({
     const language = useGettext()
     const { $gettext } = language
     const themeStore = useThemeStore()
+
+    // Only used for opening empty editor in same tab if needed
+    const configStore = useConfigStore()
+    const appsStore = useAppsStore()
+    const resourcesStore = useResourcesStore()
+    const resources = ref(resourcesStore.resources)
+    const currentFolder = ref(resourcesStore.currentFolder)
+    const spacesStore = useSpacesStore()
+    const clientService = useClientService()
+    const { getMatchingSpace } = useGetMatchingSpace()
+    const { getEditorRouteOpts } = useFileActions()
+    const { openUrl } = useWindowOpen()
+    const router = useRouter()
+    const route = useRoute()
 
     const drawIoEditor = ref<HTMLElement>()
 
@@ -155,6 +185,67 @@ export default defineComponent({
         }
       }
     }
+
+    // ------------------------------------------------------------------------
+    // Duplicated from useOpenEmptyEditor composable to force same tab opening
+    const openEditor = (
+      appFileExtension: ApplicationFileExtension,
+      space: SpaceResource,
+      resource: Resource,
+      mode: string,
+      forceSameTab: boolean = false
+    ) => {
+      const remoteItemId = isShareSpaceResource(space) ? space.id : undefined
+      const routeName = appFileExtension.routeName || appFileExtension.app
+      const routeOpts = getEditorRouteOpts(routeName, space, resource, mode, remoteItemId)
+      routeOpts.query = {
+        ...routeOpts.query,
+        // @ts-ignore
+        contextRouteName: 'files-spaces-generic'
+      }
+
+      if (!forceSameTab && configStore.options.cernFeatures) {
+        const editorRoute = router.resolve(routeOpts)
+        const editorRouteUrl = new URL(editorRoute.href, window.location.origin)
+        openUrl(editorRouteUrl.toString(), '_blank', true)
+      } else {
+        router.push(routeOpts)
+      }
+    }
+
+    const openEmptyEditor = async (
+      appId: string,
+      extension: string,
+      forceSameTab: boolean = false
+    ) => {
+      let destinationSpace = unref(currentFolder) ? getMatchingSpace(unref(currentFolder)) : null
+      let destinationFiles = unref(resources)
+      let filePath = unref(currentFolder)?.path
+
+      if (!destinationSpace || !unref(currentFolder).canCreate()) {
+        destinationSpace = spacesStore.personalSpace
+        destinationFiles = (await clientService.webdav.listFiles(destinationSpace)).children
+        filePath = ''
+      }
+
+      let fileName = $gettext('New file') + `.${extension}`
+      if (destinationFiles.some((f) => f.name === fileName)) {
+        fileName = resolveFileNameDuplicate(fileName, extension, destinationFiles)
+      }
+
+      const emptyResource = await clientService.webdav.putFileContents(destinationSpace, {
+        path: urlJoin(filePath, fileName)
+      })
+
+      const space = getMatchingSpace(emptyResource)
+      const appFileExtension = appsStore.fileExtensions.find(
+        ({ app, extension: ext }) => app === appId && ext === extension
+      )
+
+      openEditor(appFileExtension, space, emptyResource, EDITOR_MODE_EDIT, forceSameTab)
+    }
+    // End duplicated code
+    // ------------------------------------------------------------------------
 
     onMounted(() => {
       window.addEventListener('message', handleMessage)
