b57844c86360d77e9dc30d46ba40c23ee0790839
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-11-27 11:20:12 +0100

    codimd: duplicate openEmptyEditor function

--- codimd/src/App.vue
+++ codimd/src/App.vue
@@ -7,9 +7,25 @@
   </main>
 </template>
 <script lang="ts">
-import { Resource } from '@ownclouders/web-client'
-import { AppConfigObject, useRoute, useRouter, useSpacesStore } from '@ownclouders/web-pkg'
+import { isShareSpaceResource, Resource, SpaceResource, urlJoin } from '@ownclouders/web-client'
+import {
+  AppConfigObject,
+  useRoute,
+  useRouter,
+  useSpacesStore,
+  useResourcesStore,
+  useGetMatchingSpace,
+  useClientService,
+  useFileActions,
+  resolveFileNameDuplicate,
+  useAppsStore,
+  EDITOR_MODE_EDIT,
+  ApplicationFileExtension,
+  useConfigStore,
+  useWindowOpen
+} from '@ownclouders/web-pkg'
 import { defineComponent, onMounted, PropType, ref, unref } from 'vue'
+import { useGettext } from 'vue3-gettext'
 
 // https://cernbox.cern.ch/codimd -> https://cernbox.cern.ch/external-codimd/eos/user/<letter>/<username>/New file.md
 
@@ -26,9 +42,87 @@ export default defineComponent({
   },
 
   setup(props) {
+    const configStore = useConfigStore()
+    const appsStore = useAppsStore()
+    const resourcesStore = useResourcesStore()
+
+    const resources = ref(resourcesStore.resources)
+    const currentFolder = ref(resourcesStore.currentFolder)
+
     const spacesStore = useSpacesStore()
+    const clientService = useClientService()
+
+    const { getMatchingSpace } = useGetMatchingSpace()
+    const { $gettext } = useGettext()
+    const { getEditorRouteOpts } = useFileActions()
+
+    const { openUrl } = useWindowOpen()
+
     const router = useRouter()
     const route = useRoute()
+
+    // ------------------------------------------------------------------------
+    // Duplicated from useOpenEmptyEditor composable
+    // Extra features: default destination folder from app config and force same tab option
+    const openEditor = (
+      appFileExtension: ApplicationFileExtension,
+      space: SpaceResource,
+      resource: Resource,
+      mode: string,
+      forceSameTab: boolean = false
+    ) => {
+      const remoteItemId = isShareSpaceResource(space) ? space.id : undefined
+      const routeName = appFileExtension.routeName || appFileExtension.app
+      const routeOpts = getEditorRouteOpts(routeName, space, resource, mode, remoteItemId)
+      routeOpts.query = {
+        ...routeOpts.query,
+        // @ts-ignore
+        contextRouteName: 'files-spaces-generic'
+      }
+
+      if (!forceSameTab && configStore.options.cernFeatures) {
+        const editorRoute = router.resolve(routeOpts)
+        const editorRouteUrl = new URL(editorRoute.href, window.location.origin)
+        openUrl(editorRouteUrl.toString(), '_blank', true)
+      } else {
+        router.push(routeOpts)
+      }
+    }
+
+    const openEmptyEditor = async (
+      appId: string,
+      extension: string,
+      forceSameTab: boolean = false
+    ) => {
+      let destinationSpace = unref(currentFolder) ? getMatchingSpace(unref(currentFolder)) : null
+      let destinationFiles = unref(resources)
+      let filePath = unref(currentFolder)?.path
+
+      if (!destinationSpace || !unref(currentFolder).canCreate()) {
+        destinationSpace = spacesStore.personalSpace
+        destinationFiles = (await clientService.webdav.listFiles(destinationSpace)).children
+        filePath = ''
+      }
+
+      let fileName = $gettext('New file') + `.${extension}`
+
+      if (destinationFiles.some((f) => f.name === fileName)) {
+        fileName = resolveFileNameDuplicate(fileName, extension, destinationFiles)
+      }
+
+      const emptyResource = await clientService.webdav.putFileContents(destinationSpace, {
+        path: urlJoin(filePath, fileName)
+      })
+
+      const space = getMatchingSpace(emptyResource)
+      const appFileExtension = appsStore.fileExtensions.find(
+        ({ app, extension: ext }) => app === appId && ext === extension
+      )
+
+      openEditor(appFileExtension, space, emptyResource, EDITOR_MODE_EDIT, forceSameTab)
+    }
+    // End duplicated code
+    // ------------------------------------------------------------------------
 
     onMounted(async () => {
       if (!unref(route).params || !unref(route).params['driveAliasAndItem']) {
