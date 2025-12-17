bc6486043e6bc0e82af8a4f2d639e025735e2351
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-12-17 17:09:50 +0100

    data-repositories: new extension

---
+++ data-repositories/src/components/ListInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="oc-text-center">
+    <p data-testid="files-list-footer-info" class="oc-text-muted" v-text="resourceContentsText"></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useResourceContents } from '@ownclouders/web-pkg'
+
+export default defineComponent({
+  setup: () => {
+    const { resourceContentsText } = useResourceContents()
+    return { resourceContentsText }
+  }
+})
+</script>
---
+++ data-repositories/src/functions.ts
@@ -0,0 +1,59 @@
+import { buildIncomingShareResource, IncomingShareResource } from '@ownclouders/web-client'
+import {
+  useSharesStore,
+  useConfigStore,
+  useResourcesStore,
+  ClientService
+} from '@ownclouders/web-pkg'
+
+const sharesStore = useSharesStore()
+const configStore = useConfigStore()
+const resourcesStore = useResourcesStore()
+
+interface IncomingEmbeddedShareResource extends IncomingShareResource {
+  status: string
+}
+
+const firstLetterUppercase = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+const loadResources = (clientService: ClientService) => {
+  clientService.httpAuthenticated.get('sciencemesh/embedded-shares').then((response: any) => {
+    const resources: IncomingEmbeddedShareResource[] = response.data.value.map((driveItem: any) => {
+      return {
+        ...buildIncomingShareResource({
+          driveItem,
+          graphRoles: sharesStore.graphRoles,
+          serverUrl: configStore.serverUrl
+        }),
+        status: response.data.state
+          ? firstLetterUppercase(response.data.state[driveItem.id])
+          : 'unknown'
+      }
+    })
+    resourcesStore.initResourceList({
+      currentFolder: null,
+      resources
+    })
+  })
+}
+
+const processShare = (resource: IncomingEmbeddedShareResource) => {
+  console.debug('Process share clicked', {
+    name: resource.name,
+    sharedBy: resource.sharedBy,
+    sharedOn: resource.sdate,
+    status: resource.status
+  })
+  // clientService.httpAuthenticated
+  //   .post('sciencemesh', {
+  //     shareId: resource.id
+  //   })
+  //   .then(() => {
+  //     loadResources()
+  //   })
+}
+
+export { loadResources, processShare }
+export type { IncomingEmbeddedShareResource }
