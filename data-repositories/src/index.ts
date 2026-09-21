75480254fe3a8a21ffa9ddf162ac204aad16005e
Rasmus Oscar Welander <rasmus.oscar.welander@cern.ch>
2026-09-21 15:48:28 +0200

    swap icon in share list in data-repos

--- data-repositories/src/index.ts
+++ data-repositories/src/index.ts
@@ -30,7 +30,7 @@ export default defineWebApplication({
             id: `com.github.cernbox.web-extensions.nav.${appInfo.id}`,
             type: 'sidebarNav',
             navItem: {
-              icon: 'box-3',
+              icon: 'inbox',
               name: `${appInfo.name}`,
               activeFor: [{ path: `/files/${appInfo.id}` }],
               route: {
